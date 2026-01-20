2cd0cc0fdbab7b9703dabc91be5fcead38b580c9
Bharath25-tech <bharathmule1818@gmail.com>
2026-01-20 18:14:55 +0530

    SMTP

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+<body>
+    <h1>Hello, {{ $user->name }}!</h1>
+    <p>Thank you for registering at our platform. We are excited to have you onboard!</p>
+</body>
+</html>
